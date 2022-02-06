6ddd905ce12eefdb2953d68ac6cb9772f9942ff5
Viktor Mishyn <viktormishyn@gmail.com>
2022-02-06 22:21:26 +0200

    Add mock tasks to tasks component

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
