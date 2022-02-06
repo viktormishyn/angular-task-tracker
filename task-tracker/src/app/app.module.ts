18786600dc745f284d7fb522eebfdee5d5109b3e
Viktor Mishyn <viktormishyn@gmail.com>
2022-02-06 22:17:07 +0200

    Generate component tasks

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
