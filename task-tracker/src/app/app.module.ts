f2ac847f73671a6f1caac34e91db2520651bc5c9
Viktor Mishyn <viktormishyn@gmail.com>
2022-02-08 17:54:50 +0200

    Install FontAwesomeModule

--- task-tracker/src/app/app.module.ts
+++ task-tracker/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
